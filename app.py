570c25ca124290fd3e65be80c6a69e83e1debad2
Advika Kapoor <your-email@example.com>
2026-06-13 23:21:05 +0530

    Improve analytics dashboard UI and pastel theme

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+pastel_colors = [
+    "#BFA3A3",  # Dusty Rose
+    "#DCC6CC",  # Pale Mauve
+    "#D7E3D4",  # Seafoam Mist
+    "#BFC7D5",  # Blue Grey
+    "#CFC3E8",  # Powder Lilac
+    "#F2D6D0",  # Soft Blush
+]
 # =====================================
 # PAGE CONFIG
 # =====================================
@@ -20,83 +28,178 @@ st.sidebar.success(f"Foods Loaded: {len(foods_df):,}")
 # =====================================
 
 st.markdown("""
-            
+           
 <style>
+            button[data-testid="stNumberInputStepUp"],
+button[data-testid="stNumberInputStepDown"] {
+    background-color: #F6F1F1 !important;
+    color: #2E2E2E !important;
+    border: 1px solid #D8CACA !important;
+}
+
+button[data-testid="stNumberInputStepUp"]:hover,
+button[data-testid="stNumberInputStepDown"]:hover {
+    background-color: #E8D5D5 !important;
+    color: black !important;
+}
+            @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+
+html, body, [class*="css"] {
+    font-family: 'Poppins', sans-serif;
+}
+            /* ===== PASTEL SIDEBAR ===== */
+
+section[data-testid="stSidebar"] {
+    background: #E8D5D5 !important; /* Soft Blush */
+    border-right: 2px solid #D7C4C4;
+}
+
+section[data-testid="stSidebar"] * {
+    color: #6F5A64 !important; /* Vintage Mauve */
+}
+
+section[data-testid="stSidebar"] .stButton > button {
+    background: #DCC6CC !important; /* Pale Mauve */
+    color: #6F5A64 !important;
+    border: none !important;
+    border-radius: 12px !important;
+}
+
+section[data-testid="stSidebar"] .stButton > button:hover {
+    background: #CDB7C5 !important;
+}
             .main .block-container {
     max-width: 1400px;
     padding-top: 2rem;
     padding-left: 3rem;
     padding-right: 3rem;
 }
-            .metric-card{
-    background: linear-gradient(135deg,#1f2937,#111827);
+.metric-card{
+    background: linear-gradient(
+        135deg,
+        #E8D5D5,
+        #F6EEEE
+    );
     padding:20px;
-    border-radius:15px;
+    border-radius:20px;
     text-align:center;
-    border:1px solid #374151;
-    box-shadow:0px 4px 15px rgba(0,0,0,0.4);
+    border:2px solid #DCC6CC;
+    box-shadow:0 8px 20px rgba(0,0,0,0.08);
 }
-
 .metric-title{
-    color:#9ca3af;
-    font-size:15px;
+    color:#7D6B73;
+    font-size:16px;
+    font-weight:600;
 }
-
 .metric-value{
-    color:white;
-    font-size:32px;
-    font-weight:bold;
+    color:#6F5A64;
+    font-size:36px;
+    font-weight:700;
 }
 
 .stApp {
-    background-color: #0E1117;
-}
-
-.stApp {
-    background-color: #0E1117;
+    background: #BFA3A3;
 }
 
 /* Buttons */
 .stButton > button {
-    background: linear-gradient(90deg,#ff4b4b,#ff8c00);
-    color: white;
+    background: #DCC6CC;
+    color: #6F5A64;
     border: none;
-    border-radius: 10px;
+    border-radius: 14px;
     height: 3rem;
     width: 100%;
-    font-size: 18px;
-    font-weight: bold;
+    font-size: 16px;
+    font-weight: 600;
+    box-shadow: 0 4px 10px rgba(0,0,0,0.08);
+}
+
+.stButton > button:hover {
+    background: #CDB7C5;
+    transform: translateY(-2px);
+    transition: 0.3s;
 }
 
 /* Cards */
 .card {
-    background-color: #1e2430;
-    padding: 20px;
-    border-radius: 15px;
-    margin-bottom: 20px;
+    background: #F6EEEE;
+    padding:20px;
+    border-radius:20px;
+    margin-bottom: 25px;
+    border: 1px solid #DCC6CC; /* Pale Mauve */
+    box-shadow: 0 6px 18px rgba(0,0,0,0.08);
 }
 
 /* Recommendation Cards */
 .rec-card {
-    background: linear-gradient(135deg,#232526,#414345);
+    background: #E8D5D5;   /* Soft Blush */
     padding: 20px;
-    border-radius: 15px;
+    border-radius: 20px;
     margin-bottom: 20px;
-    border-left: 5px solid orange;
+    border-left: 6px solid #A67C87; /* Vintage Mauve */
     min-height: 320px;
+    box-shadow: 0 8px 20px rgba(0,0,0,0.08);
+}
+.rec-card h3 {
+    color: #6F5A64 !important;   /* Vintage Mauve */
+    font-weight: 700;
+}
+
+.rec-card p {
+    color: #4E5968 !important;   /* Blue Grey */
+    font-size: 17px;
+    font-weight: 500;
 }
 .rec-card:hover {
-    transform: translateY(-5px);
-    transition: 0.3s;
+    transform: translateY(-8px);
+    transition: 0.3s ease;
 }
-
+.chart-card{
+    background:#F6EEEE;
+    padding:20px;
+    border-radius:20px;
+    box-shadow:0 6px 18px rgba(0,0,0,0.08);
+    margin-bottom:20px;
+}
 /* Section Headers */
 .section-title {
-    color: #FFD700;
-    font-size: 32px;
-    font-weight: bold;
+    color:#A67C87;
+    font-size:36px;
+    font-weight:700;
+    letter-spacing:0.5px;
+}
+/* INPUT BOXES */
+
+.stTextInput input,
+.stNumberInput input,
+.stTextArea textarea {
+    background: #F6EEEE !important;
+    color: #4E5968 !important;
+    border: 2px solid #DCC6CC !important;
+    border-radius: 12px !important;
 }
 
+/* DROPDOWNS */
+
+.stSelectbox > div > div,
+.stMultiSelect > div > div {
+    background: #F6EEEE !important;
+    color: #4E5968 !important;
+    border: 2px solid #DCC6CC !important;
+    border-radius: 12px !important;
+}
+button[data-testid="stNumberInputStepUp"],
+button[data-testid="stNumberInputStepDown"] {
+    background-color: #DDD6F3 !important;
+    color: #2E2E2E !important;
+    border: 1px solid #CDB7C5 !important;
+}
+
+button[data-testid="stNumberInputStepUp"]:hover,
+button[data-testid="stNumberInputStepDown"]:hover {
+    background-color: #E8D5D5 !important;
+    color: black !important;
+}
 </style>
 """, unsafe_allow_html=True)
 
@@ -108,8 +211,14 @@ st.markdown("""
 <h1 style='text-align:center;
 padding:20px;
 border-radius:15px;
-background: linear-gradient(90deg,#ff4b4b,#ff8c00,#ffd700);
-color:white;'>
+background: linear-gradient(
+90deg,
+#DCC6CC,
+#E8D5D5,
+#F6EEEE,
+#D7E3D4
+);
+color:#6F5A64;
 🎯 Smart Recommendation Hub
 </h1>
 """, unsafe_allow_html=True)
@@ -155,7 +264,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1,1])
 
 with col1:
     name = st.text_input("Enter your Name")
@@ -436,54 +545,98 @@ with col1:
     highest_protein = recommended_foods.loc[
         recommended_foods["protein_g"].idxmax()
     ]
-    st.success(
-        f"💪 Highest Protein\n\n{highest_protein['food_name'][:30]}"
-    )
+    st.markdown(f"""
+<div style="
+background:#E8F1EC;
+padding:20px;
+border-radius:15px;
+color:#2E8B7A;
+font-weight:600;
+">
+💪 Highest Protein<br><br>
+{highest_protein['food_name'][:30]}
+</div>
+""", unsafe_allow_html=True)
 
 with col2:
     lowest_calorie = recommended_foods.loc[
         recommended_foods["calories"].idxmin()
     ]
-    st.info(
-        f"🔥 Lowest Calories\n\n{lowest_calorie['food_name'][:30]}"
-    )
-
+    st.markdown(f"""
+<div style="
+background:#EEEAF7;
+padding:20px;
+border-radius:15px;
+color:#6A5ACD;
+font-weight:600;
+">
+🔥 Lowest Calories<br><br>
+{lowest_calorie['food_name'][:30]}
+</div>
+""", unsafe_allow_html=True)
 with col3:
     healthiest = recommended_foods.loc[
         recommended_foods["health_score"].idxmax()
     ]
-    st.warning(
-        f"❤️ Healthiest Choice\n\n{healthiest['food_name'][:30]}"
-    )
+    st.markdown(f"""
+<div style="
+background:#FDF1E8;
+padding:20px;
+border-radius:15px;
+color:#C57A1F;
+font-weight:600;
+">
+💖 Healthiest Choice<br><br>
+{healthiest['food_name'][:30]}
+</div>
+""", unsafe_allow_html=True)
 st.subheader(
     f"📋 Recommendation Summary for {name if name else 'Guest User'}"
 )
 
-st.info(
-    f"""
-🎯 Total Recommendations Found: {len(recommended_foods)}
+st.markdown(f"""
+<div style="
+background:#EEF3FB;
+padding:25px;
+border-radius:18px;
+color:#2F5DA8;
+font-size:18px;
+line-height:2;
+">
 
-🥇 Best Match: {recommended_foods.iloc[0]['food_name']}
+🎯 Total Recommendations Found: {len(recommended_foods)}<br>
 
+🥇 Best Match: {recommended_foods.iloc[0]['food_name']}<br>
 
-📂 Category: {recommended_foods.iloc[0]['food_category']}
+📁 Category: {recommended_foods.iloc[0]['food_category']}<br>
 
 ❤️ Health Score: {recommended_foods.iloc[0]['health_score']}
-"""
-)
+
+</div>
+""", unsafe_allow_html=True)
 top_food = recommended_foods.iloc[0]
 
-st.success(
-    f"""
-🌟 TODAY'S BEST RECOMMENDATION
+st.markdown(f"""
+<div style="
+background:#FAF7EA;
+padding:25px;
+border-radius:18px;
+color:#C58A00;
+font-size:18px;
+line-height:2;
+font-weight:600;
+">
 
-🥇 {top_food['food_name']}
+🌟 TODAY'S BEST RECOMMENDATION<br><br>
 
-📁 Category: {top_food['food_category']}
+🥇 {top_food['food_name']}<br>
+
+📁 Category: {top_food['food_category']}<br>
 
 ❤️ Health Score: {top_food['health_score']}
-"""
-)
+
+</div>
+""", unsafe_allow_html=True)
 search_food = st.text_input(
     "🔍 Search Recommended Foods",
     placeholder="Type food name..."
@@ -644,93 +797,264 @@ with col3:
     <p>Score: {healthiest['health_score']}</p>
     </div>
     """, unsafe_allow_html=True)
+st.markdown("""
+<style>
+[data-testid="stDataFrame"] {
+    background-color: #EEEAF7 !important;
+}
+
+[data-testid="stDataFrame"] table {
+    background-color: #EEEAF7 !important;
+    color: black !important;
+}
+
+[data-testid="stDataFrame"] th {
+    background-color: #DCC6CC !important;
+    color: black !important;
+    font-weight: bold !important;
+}
+
+[data-testid="stDataFrame"] td {
+    background-color: #EEEAF7 !important;
+    color: black !important;
+}
+</style>
+""", unsafe_allow_html=True)
 st.subheader("📊 Food Comparison Table")
-st.dataframe(
-        recommended_foods[
-            [
-                "food_name",
-                "food_category",
-                "calories",
-                "protein_g",
-                "health_score"
-            ]
+styled_df = recommended_foods[
+    [
+        "food_name",
+        "food_category",
+        "calories",
+        "protein_g",
+        "health_score"
+    ]
+].style\
+.set_properties(**{
+    'background-color': '#EEEAF7',
+    'color': 'black',
+    'border-color': '#DCC6CC'
+})\
+.set_table_styles([
+    {
+        'selector': 'th',
+        'props': [
+            ('background-color', '#E8D5D5'),
+            ('color', 'black'),
+            ('font-weight', 'bold')
         ]
-    )
+    },
+    {
+        'selector': '.row_heading',
+        'props': [
+            ('background-color', '#DCC6CC'),
+            ('color', 'black')
+        ]
+    },
+    {
+        'selector': '.blank',
+        'props': [
+            ('background-color', '#DCC6CC'),
+        ]
+    }
+])
+
+st.table(styled_df)
 st.caption(
     "Compare calories, protein, carbs and health scores across recommendations."
 )
 csv = recommended_foods.to_csv(index=False)
-
+st.markdown("""
+<style>
+div.stDownloadButton > button {
+    background-color: #DDD6F3;
+    color: #2E2E2E;
+    border: 2px solid #CDB7C5;
+    border-radius: 12px;
+    font-weight: 600;
+}
+div.stDownloadButton > button:hover {
+    background-color: #E8D5D5;
+    color: black;
+}
+</style>
+""", unsafe_allow_html=True)
 st.download_button(
     label="📥 Download Recommendations CSV",
     data=csv,
     file_name="food_recommendations.csv",
     mime="text/csv"
 )
-st.success(
-        f"Hello {name}! Here are your personalized recommendations."
-    )
-st.subheader("📊 Food Categories Distribution")
-st.caption("Distribution of recommended foods by category")
-category_counts = recommended_foods["food_type"].value_counts()
+st.markdown(f"""
+<div style="
+background-color:#EEEAF7;
+padding:20px;
+border-radius:15px;
+border-left:6px solid #CDB7C5;
+color:#2E2E2E;
+font-size:20px;
+font-weight:600;
+">
+✨ Hello {name}! Here are your personalized recommendations.
+</div>
+""", unsafe_allow_html=True)
+st.markdown("""
+<h2 style='
+color:#6F5A64;
+font-size:38px;
+font-weight:700;
+margin-top:20px;
+margin-bottom:10px;
+'>
+📈 Analytics Dashboard
+</h2>
+""", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
 
-st.bar_chart(category_counts, height=400)
-import plotly.express as px
+col1, col2 = st.columns([1,1])
 
-st.subheader("🥧 Category Percentage")
+with col1:
+    st.markdown("""
+<h3 style='color:#A67C87;font-size:28px;font-weight:600;'>
+📊 Food Categories Distribution
+</h3>
+""", unsafe_allow_html=True)
+    st.caption("Distribution of recommended foods by category")
+    
 
-st.subheader("💪 Top 5 Protein Comparison")
 
-protein_fig = px.bar(
-    recommended_foods.head(5),
-    x="food_name",
-    y="protein_g",
-    title="💪 Top 5 Protein Rich Foods",
-    text="protein_g"
+    category_counts = recommended_foods["food_type"].value_counts()
+
+    category_df = category_counts.reset_index()
+    category_df.columns = ["Category", "Count"]
+    category_fig = px.bar(
+    category_df,
+    x="Category",
+    y="Count",
+    text="Count",
+    color_discrete_sequence=["#D8A7B1"]  # Dusty Rose
 )
 
-protein_fig.update_layout(
-    xaxis_title="Food",
-    yaxis_title="Protein (g)",
-    height=500
+    category_fig.update_layout(
+    height=280,
+    width=450,
+    plot_bgcolor="#F8F4F1",
+    paper_bgcolor="#F8F4F1",
+    font_color="#6F5A64",
+    xaxis_title="",
+    yaxis_title=""
 )
 
-protein_fig.update_traces(
-    textposition="outside"
-)
-st.plotly_chart(protein_fig, use_container_width=True)
-st.subheader("🔥 Top 5 Calories Comparison")
+    st.plotly_chart(category_fig, use_container_width=True)
+with col2:
+    st.markdown("""
+<h3 style='color:#A67C87;font-size:28px;font-weight:600;'>
+🧭 Category Percentage
+</h3>
+""", unsafe_allow_html=True)
 
-calories_fig = px.bar(
-    recommended_foods.head(5),
-    x="food_name",
-    y="calories",
-    title="🔥 Top 5 Calories Comparison",
-    text="calories"
-)
-
-calories_fig.update_layout(
-    xaxis_title="Food",
-    yaxis_title="Calories",
-    height=500
-)
-
-calories_fig.update_traces(
-    textposition="outside"
-)
-
-st.plotly_chart(calories_fig, use_container_width=True)
-fig = px.pie(
+    fig = px.pie(
     values=category_counts.values,
     names=category_counts.index,
-    hole=0.4
+    hole=0.5,
+    color_discrete_sequence=pastel_colors
 )
-fig.update_traces(
-    textposition="inside",
-    textinfo="percent+label"
-)
-st.plotly_chart(fig, use_container_width=True)   
 
+    fig.update_layout(
+    height=350,
+    plot_bgcolor="#F6EEEE",
+    paper_bgcolor="#F6EEEE",
+    font_color="#6F5A64"
+)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+col3, col4 = st.columns(2)
+
+with col3:
+
+    st.markdown("""
+<h3 style='color:#A67C87;font-size:28px;font-weight:600;'>
+💪 Top 5 Protein Comparison
+</h3>
+""", unsafe_allow_html=True)
+    
+    chart_data = recommended_foods.head(5).copy()
+    chart_data["short_name"] = (
+        chart_data["food_name"].str[:20] + "..."
+    )
+    protein_fig = px.bar(
+        chart_data,
+        x="short_name",
+        y="protein_g",
+        color="short_name",
+        color_discrete_sequence=[
+         "#D8A7B1",  # Dusty Rose
+         "#E8CFCF",  # Soft Blush
+         "#DCC6CC",  # Pale Mauve
+         "#CDB7C5",  # Powder Lilac
+         "#AFC8B8"   # Seafoam Mist
+    ],
+        text="protein_g"
+    )
+    protein_fig.update_layout(
+    height=400,
+    xaxis_tickangle=-30,
+    coloraxis_showscale=False,
+    plot_bgcolor="#F6EEEE",
+    paper_bgcolor="#F6EEEE",
+    font_color="#6F5A64",
+    showlegend=False,
+    xaxis_showgrid=False,
+    yaxis_gridcolor="#E8DADA",
+)
+    protein_fig.update_traces(
+    textposition="outside",
+    textfont_size=14,
+    marker_line_width=0
+)
+    st.plotly_chart(protein_fig, use_container_width=True)
+
+with col4:
+
+    st.markdown("""
+<h3 style='color:#A67C87;font-size:28px;font-weight:600;'>
+🔥 Top 5 Calories Comparison
+</h3>
+""", unsafe_allow_html=True)
+
+    calories_fig = px.bar(
+    chart_data,
+    x="short_name",
+    y="calories",
+    text="calories",
+    color_discrete_sequence=[
+    "#D8A7B1",  # Dusty Rose
+    "#E8CFCF",  # Soft Blush
+    "#DCC6CC",  # Pale Mauve
+    "#CDB7C5",  # Powder Lilac
+    "#AFC8B8"   # Seafoam Mist
+],
+)
+
+    calories_fig.update_layout(
+    height=400,
+    xaxis_tickangle=-30,
+    coloraxis_showscale=False,
+    plot_bgcolor="#F6EEEE",
+    paper_bgcolor="#F6EEEE",
+    font_color="#6F5A64",
+    showlegend=False,
+    xaxis_showgrid=False,
+    yaxis_gridcolor="#E8DADA",
+)
+    calories_fig.update_traces(
+    textposition="outside",
+    textfont_size=14,
+    marker_line_width=0
+)
+
+    st.plotly_chart(calories_fig, use_container_width=True)
     
     # =====================================
     # USER PROFILE
